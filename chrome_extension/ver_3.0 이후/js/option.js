eb7b7ea8b1217deb5e73e1e8e8af118a38baa4c5
Yoon Jeongyeon <jyeon.yoon59@gmail.com>
2021-06-01 20:25:31 +0900

    chrome extenstion [옵션 개수 추가]

--- chrome_extension/ver_3.0 이후/js/option.js
+++ chrome_extension/ver_3.0 이후/js/option.js
@@ -1,9 +1,12 @@
 document.getElementById('save').addEventListener('click', () => {
-    let likeColorRed = document.getElementById('Red').checked;
-    let likeColorGreen = document.getElementById('Green').checked;
-    let likeColorBlue = document.getElementById('Blue').checked;
+    let option1 = document.getElementById('op1').checked;
+    let option2 = document.getElementById('op2').checked;
+    let option3 = document.getElementById('op3').checked;
+    let option4 = document.getElementById('op4').checked;
+    let option5 = document.getElementById('op5').checked;
+    let option6 = document.getElementById('op6').checked;
 
-    chrome.storage.sync.set({['keyRed']: likeColorRed, ['keyGreen']: likeColorGreen, ['keyBlue']: likeColorBlue}, function(){
+    chrome.storage.sync.set({['op1']: option1, ['op2']: option2, ['op3']: option3, ['op4']: option4, ['op5']: option5, ['op6']: option6}, function(){
         alert("save!");
     })
 });
