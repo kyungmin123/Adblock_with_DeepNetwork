dd0590cb5b7d45500630a4ff962e94b59e6a26bd
Yoon Jeongyeon <jyeon.yoon59@gmail.com>
2021-06-12 01:57:09 +0900

    div 태그 접근하는 부분, 속도 개선 부분 추가

--- chrome_extension/ver_3.0 이후/program/js/adblock.js
+++ chrome_extension/ver_3.0 이후/program/js/adblock.js
@@ -1,60 +1,123 @@
-var option1;
-var option2;
-var option3;
-var option4;
-
-function removeAds(){
+function removeImageAds(){
     let imgs = document.getElementsByTagName("img");
     chrome.storage.sync.get({['op1']: true, ['op2']: true, ['op3']: true, ['op4']: true}, function(items){
         option1 = items.op1;
         option2 = items.op2;
         option3 = items.op3;
         option4 = items.op4;
-        option5 = items.op5;
         for(let i = 0; i < imgs.length; i++){
+            if (!imgs[i].src)
+                continue;
             chrome.runtime.sendMessage({url: imgs[i].src},
                 function (response) {
-                    //console.log(`current option1 is ${option1}`);
                     let imgclass = response["class"];
-                    if(imgclass === "a" && option1){
-                        imgs[i].setAttribute("style", "display: none;");
-                        //imgs[i].setAttribute("style", "border: 10 solid red");
-                        callback(response);
+                    switch(imgclass){
+                        case "a":
+                            if (option1){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid red");
+                                callback(response);
+                            }
+                            break;
+                        case "b":
+                            if (option2){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid orange");
+                                callback(response);
+                            }
+                            break;
+                        case "d":
+                            if (option3){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid yellow");
+                                callback(response);
+                            }
+                            break;
+                        case "e":
+                            if (option4){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid green");
+                                callback(response);
+                            }
+                            break;
+                        case "absolute":
+                            imgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid purple");
+                            callback(response);
+                            break;
+                        case "n":
+                            //imgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid blue");
+                            callback(response);
+                            break;
+                        default:
+                            mgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid purple");
+                            callback(response);
+                            break;
                     }
-                    else if(imgclass === "b" && option2){
-                        imgs[i].setAttribute("style", "display: none;");
-                        //imgs[i].setAttribute("style", "border: 10 solid orange");
-                        callback(response);
+                }
+            );
+        }
+    });
+}
+
+function removeDivAds(){
+    let imgs = document.getElementsByTagName("div");
+    chrome.storage.sync.get({['op1']: true, ['op2']: true, ['op3']: true, ['op4']: true}, function(items){
+        for(let i = 0; i < imgs.length; i++){
+            let style = imgs[i].currentStyle || window.getComputedStyle(imgs[i], false);
+            bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+            if(!bi)
+                continue;
+            chrome.runtime.sendMessage({url: bi},
+                function (response) {
+                    let imgclass = response["class"];
+                    switch(imgclass){
+                        case "a":
+                            if (option1){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid red");
+                                callback(response);
+                            }
+                            break;
+                        case "b":
+                            if (option2){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid orange");
+                                callback(response);
+                            }
+                            break;
+                        case "d":
+                            if (option3){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid yellow");
+                                callback(response);
+                            }
+                            break;
+                        case "e":
+                            if (option4){
+                                imgs[i].setAttribute("style", "display: none;");
+                                //imgs[i].setAttribute("style", "border: 10 solid green");
+                                callback(response);
+                            }
+                            break;
+                        case "absolute":
+                            imgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid purple");
+                            callback(response);
+                            break;
+                        case "n":
+                            //imgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid blue");
+                            callback(response);
+                            break;
+                        default:
+                            mgs[i].setAttribute("style", "display: none;");
+                            //imgs[i].setAttribute("style", "border: 10 solid purple");
+                            callback(response);
+                            break;
                     }
-                    else if(imgclass === "d" && option3){
-                        imgs[i].setAttribute("style", "display: none;");
-                        //imgs[i].setAttribute("style", "border: 10 solid yellow");
-                        callback(response);
-                    }
-                    else if(imgclass === "e" && option4){
-                        imgs[i].setAttribute("style", "display: none;");
-                        //imgs[i].setAttribute("style", "border: 10 solid green");
-                        callback(response);
-                    }
-                    else if(imgclass === "n"){
-                        //imgs[i].setAttribute("style", "border: 10 solid blue");
-                        callback(response);
-                    }
-                    else if (imgclass === "absolute"){
-                        imgs[i].setAttribute("style", "display: none;");
-                        //imgs[i].setAttribute("style", "border: 10 solid purple");
-                        callback(response);
-                    }
-                    /*
-                    if (option1 === false){
-                        imgs[i].setAttribute("style", "display: none;");
-                        callback(response);
-                    }
-                    else if (option1 === true){
-                        imgs[i].setAttribute("style", "opacity: 0.5;");
-                        callback(response);
-                    }
-                    */
                 }
             );
         }
@@ -62,5 +125,6 @@ function removeAds(){
 }
 
 window.onload = function(){
-    removeAds();
+    removeImageAds();
+    removeDivAds();
 }
